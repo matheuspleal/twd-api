b60cc287d606acf776a624fb692728dc9a1c8b88
Matheus Leal <matheusspleal@gmail.com>
2024-12-24 10:57:44 -0300

    test: ensure email vo works correctly

---
+++ test/modules/users/domain/value-objects/email-vo.spec.ts
@@ -0,0 +1,41 @@
+import { EmailVO } from '@/modules/users/domain/value-objects/email-vo'
+
+describe('EmailVO', () => {
+  let sut: EmailVO
+
+  it('should be able to returns false when email has a invalid format', () => {
+    const fakeInvalidEmail = 'fake-invalid-email'
+    sut = new EmailVO({ value: fakeInvalidEmail })
+
+    const isValid = sut.isValid()
+
+    expect(isValid).toBe(false)
+    expect(sut.toValue()).toEqual(fakeInvalidEmail)
+    expect(sut.toString()).toEqual(fakeInvalidEmail)
+  })
+
+  it.each(['fake-invalid-email', 'fake-invalid-email@'])(
+    'should be able to returns false when email does not have a valid top-level domain',
+    (email) => {
+      sut = new EmailVO({ value: email })
+
+      const isValid = sut.isValid()
+
+      expect(isValid).toBe(false)
+    },
+  )
+
+  it.each([
+    'fake-valid-email@fake-domain.com',
+    'fake-valid-email@fake-domain.org',
+  ])(
+    'should be able to returns true when email has a valid top-level domain',
+    (email) => {
+      sut = new EmailVO({ value: email })
+
+      const isValid = sut.isValid()
+
+      expect(isValid).toBe(true)
+    },
+  )
+})
